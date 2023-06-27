6eea8807cba67d7d1e1f5b3b668b26a7e360c043
Nino Helfenstein <nino.helfenstein@hispeed.ch>
2023-06-27 22:17:09 +0200

    caesar decryption

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 let caesar = `
-    <p class="info">You can only use normal characters and numbers. Space will remain a space</p>
-    <input type="number" name="caesarKey" id="caesarKey" placeholder="key">
-    <div class="EncodeWrapper">
-      <input type="text" id="caesarInputEncode" placeholder="Text to encode">
+<p class="info">You can only use normal characters and numbers. Space will remain a space. The key can only be a natural number</p>
+<input type="number" name="caesarKey" id="caesarKey" class="key"  placeholder="key" min=0>
+    <div class="caesarEncodeWrapper">
+      <textarea id="caesarInputEncode" rows="4" cols="50" placeholder="Text to encode"></textarea>
       <button type="button" onclick="caesarEncode()">Encode</button>
     </div>
     <br>
     <div class="caesarDecodeWrapper">
-      <input type="text" id="caesarInputDecode" placeholder="Text to decode">
+      <textarea id="caesarInputDecode" rows="4" cols="50" placeholder="Text to decode"></textarea>
       <button type="button" onclick="caesarDecode()">Decode</button>
     </div>
     <div class="OutputWrapper" onclick="copyOutput()">
-    <div id="Output" class="Output"></div>
+      <pre id="Output" class="Output"></pre>
       <img class="copyOutput" src="src/copy.png" alt"copyText-Icon">
     </div>
   `;
-  
+
 const caesarString =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
 function caesarEncode() {
+  // Da bechömemer de text und de key(de machemer no en float) nacher luegemer öb beides usgfühlt isch udn denn wird gluegt öbs spezial zeiche hed.
+  // falls es speziall zeiche hed wird en error gmacht. nacher wird gluegt öb de key valid isch und denn wird res difiniert.
+  // nacher wird d'längi vom text gloopt und bi jedem zeiche glugt öbs space isch und sucht bim z'gliche zeiche plus de key vom caesarString gno.
+  // am schluss wird no das zeiche res hinzuegfüegt. das ganze wird denn mit innerhtml is dokument ihgfüegt.
   let text = document.getElementById("caesarInputEncode").value;
-  if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]+/.test(text)) {
-    //TODO
-    error("contains special characters, won't work.");
+  let key = parseFloat(document.getElementById("caesarKey").value);
+  if (!text || !key) {
+    error("You need a Key and a Text to encode");
     return;
   }
-  let key = parseInt(document.getElementById("caesarKey").value);
+  if (/[^a-zA-Z0-9\s]/g.test(text)) {
+    error("Contains special characters, won't work.");
+    return;
+  }
+  if (key % 1 !== 0 || key < 0) {
+    error("Key isn't a natural number");
+    return;
+  }
   let res = "";
   for (let i = 0; i < text.length; i++) {
     let caesarStringIndex = caesarString.indexOf(text.charAt(i));
-    let char = caesarStringIndex + key;
-    char = char % caesarString.length;
-    res = res.concat(caesarString.charAt(char));
+    if (/\s/.test(text.charAt(i))) {
+      console.log("Current character is a space");
+      res = res.concat(text.charAt(i));
+    } else {
+      let char = caesarStringIndex + key;
+      char = char.mod(caesarString.length);
+      res = res.concat(caesarString.charAt(char));
+    }
   }
   document.getElementById("logo").src = "src/cryptyLogoTransparent.png";
   document.body.classList.remove("error");
@@ -40,19 +56,32 @@ function caesarEncode() {
 }
 
 function caesarDecode() {
+  // Da passiert s'gliche wie bim Encode eifach das de Key minus grechned wird und ned plus. 
   let text = document.getElementById("caesarInputDecode").value;
-  if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]+/.test(text)) {
-    //TODO
-    alert("contains special characters, won't work. ");
+  let key = parseFloat(document.getElementById("caesarKey").value);
+  if (!text || !key) {
+    error("You need a Key and a Text to encode");
     return;
   }
-  let key = parseInt(document.getElementById("caesarKey").value);
+  if (/[^a-zA-Z0-9\s]/g.test(text)) {
+    error("Contains special characters, won't work.");
+    return;
+  }
+  if (key % 1 !== 0 || key < 0) {
+    error("Key isn't a natural number");
+    return;
+  }
   let res = "";
   for (let i = 0; i < text.length; i++) {
     let caesarStringIndex = caesarString.indexOf(text.charAt(i));
-    let char = caesarStringIndex - key;
-    char = char % caesarString.length;
-    res = res.concat(caesarString.charAt(char));
+    if (/\s/.test(text.charAt(i))) {
+      console.log("Current character is a space");
+      res = res.concat(text.charAt(i));
+    } else {
+      let char = caesarStringIndex - key;
+      char = char.mod(caesarString.length);
+      res = res.concat(caesarString.charAt(char));
+    }
   }
   document.getElementById("logo").src = "src/cryptyLogoTransparent.png";
   document.body.classList.remove("error");
@@ -127,3 +156,9 @@ function copyOutput() {
   const output = document.getElementById("Output");
   navigator.clipboard.writeText(output.innerHTML);
 }
+
+// TO WORK OUT THE MODULO BUG
+// web.archive.org/web/20090717035140if_/javascript.about.com/od/problemsolving/a/modulobug.htm
+https: Number.prototype.mod = function (n) {
+  return ((this % n) + n) % n;
+};
