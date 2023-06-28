cd0c1d9615edad340f6335a40ad8aac0b171845c
Benedict Brück <Benedict.brueck@twofold.swiss>
2023-06-28 09:13:22 +0200

    Vigenere encode & decode

--- script.js
+++ script.js
@@ -16,22 +16,70 @@ let option1 = `
     </div>
   `;
 
-let option2 = `
-    <p class="info">Info Text</p>
-    <input type="number" name="option2Key" id="option2Key" placeholder="key">
-    <div class="EncodeWrapper">
-      <input type="text" id="option2InputEncode" placeholder="Text to encode">
-      <button type="button" onclick="option2Encode()">Encode</button>
-    </div>
-    <br>
-    <div class="option2DecodeWrapper">
-      <input type="text" id="option2InputDecode" placeholder="Text to decode">
-      <button type="button" onclick="option2Decode()">Decode</button>
-    </div>
-    <div id="option2Output" class="Output" onclick="copyOutput(option1Output)">
-      <img class="copyOutput" src="src/copy.png" alt"copyText-Icon">
-    </div>
-  `;
+  let option2 = `
+  <p class="info">Vigenère-Verschlüsselung</p>
+  <input type="text" name="VigenèreKey" id="VigenèreKey" placeholder="key">
+  <div class="VigenèreEncodeWrapper">
+    <input type="text" id="VigenèreInputEncode" placeholder="Text to encode">
+    <button type="button" onclick="VigenèreEncode()">Encode</button>
+  </div>
+  <br>
+  <div class="VigenèreDecodeWrapper">
+    <input type="text" id="VigenèreInputDecode" placeholder="Text to decode">
+    <button type="button" onclick="VigenèreDecode()">Decode</button>
+  </div>
+  <div id="VigenèreOutput" class="Output" onclick="copyOutput(VigenèreOutput)">
+    <input type="text" id="VigenèreOutputA" placeholder="Text to decode">
+    <img class="copyOutput" src="src/copy.png" alt"copyText-Icon">
+  </div>
+`;
+
+function VigenèreEncode() {
+  //read the DOM Input & Key, set EncyptedText & KeyIndex to uppercase
+  let input = document.getElementById("VigenèreInputEncode").value.toUpperCase();
+  let key = document.getElementById("VigenèreKey").value.toUpperCase();
+  let encryptedText = '';
+  let keyIndex = 0;
+  //Loop over each character via KeyCharacter
+  for (let i = 0; i < input.length; i++) {
+    let charCode = input.charCodeAt(i);
+    //if charset grösser gleich 65 & charcode kleiner als 90
+    if (charCode >= 65 && charCode <= 90) {
+      let keyChar = key.charCodeAt(keyIndex % key.length) - 65;
+      let encryptedChar = String.fromCharCode(((charCode - 65 + keyChar) % 26) + 65);
+      encryptedText += encryptedChar;
+      keyIndex++;
+    } else {
+      encryptedText += input.charAt(i);
+    }
+  }
+  console.log(encryptedText)
+  document.getElementById("VigenèreInputDecode").value = encryptedText;
+}
+
+function VigenèreDecode() {
+  //read the DOM Input & Key, set EncyptedText & KeyIndex
+  let input = document.getElementById("VigenèreInputDecode").value.toUpperCase();
+  let key = document.getElementById("VigenèreKey").value.toUpperCase();
+  console.log(1)
+  let decryptedText = '';
+  let keyIndex = 0;
+  //Loop over each character via KeyCharacter
+  for (let i = 0; i < input.length; i++) {
+    let charCode = input.charCodeAt(i);
+    //if charset grösser gleich 65 & charcode kleiner als 90
+    if (charCode >= 65 && charCode <= 90) {
+      let keyChar = key.charCodeAt(keyIndex % key.length) - 65;
+      let decryptedChar = String.fromCharCode(((charCode - 65 - keyChar + 26) % 26) + 65);
+      decryptedText += decryptedChar;
+      keyIndex++;
+    } else {
+      decryptedText += input.charAt(i);
+    }
+  }
+  //return into Output-element
+  document.getElementById("VigenèreOutput").value = decryptedText;
+}
 
 let option3 = `
     <p class="info">Info Text</p>
